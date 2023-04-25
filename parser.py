78700731ce253706b5a44d2a45d7744287e90f59
XxX_Nagibator228_XxX <61377502+DiVik-htelter@users.noreply.github.com>
2023-04-25 17:21:41 +0600

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,60 @@
+from bs4 import BeautifulSoup # импортируем библиотеку BeautifulSoup
+import requests # импортируем библиотеку requests
+
+
+
+
+
+def farmakopeika(name):
+    url = 'https://farmakopeika.ru/search?query=' + name # передаем необходимы URL адрес
+    page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+    #print(page.status_code) # смотрим ответ
+    soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
+
+    block = soup.findAll('div',id="products-app") # находим  контейнер с нужным классом
+    description = ''
+    for data in block: # проходим циклом по содержимому контейнера
+        if data.find('div'): # находим тег
+            description = data.text # записываем в переменную содержание тега    
+    
+    
+    return sFarm(description)
+
+def sFarm(s):
+    s = s.replace('В корзину','')
+    for i in range(4):
+        s = s.replace('\n\n','\n')
+    s = s.replace('\nнет',' нет')
+    s = s.replace('\nесть',' есть')
+    s = s.replace('В аптеках:\n','В аптеках: ')
+    s = s.replace('от','Цена от: ')
+    s = s.replace('\n\n','\n')
+
+    s =s.split(sep = '\n')
+
+    for i in range(len(s)):
+        if i%4==0:
+            s[i]+='\n'
+    s = "\n".join(s)
+    
+    print('\n',s,'-'*35)
+    if s == '':
+        return 'Ничего не нашлось'
+    return s
+
+'''
+
+def parser_apteka(name):
+    url = 'https://apteka.ru/search/?q=' + name # передаем необходимы URL адрес
+    page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+    #print(page.status_code) # смотрим ответ
+    soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
+
+    block = soup.findAll('div',class_="cards-list-sort-filter") # находим  контейнер с нужным классом
+    description = ''
+    for data in block: # проходим циклом по содержимому контейнера
+        if data.find('a'): # находим тег
+            description = data.text # записываем в переменную содержание тега
+
+   # print(description)
+'''
